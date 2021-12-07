ff7325a2b348a695e2c403de16a5a62ad141311e
smruti900 <smruti1100@gmail.com>
2021-12-07 23:33:33 +0530

    Viewdata and viewdata attribute in application

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,24 +10,38 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+        [ViewData]
+        public string Title { get; set; }
+        [ViewData]
+        public BookModel book { get; set; }
         public ViewResult Index()
         {
+            Title = "This the home title";
+            CustomProperty = "Custom Value";
+
+            book = new BookModel() { Id = 1, Author = "abhisheka" };
+            //ViewData["property1"] = "smruti";
+
+            //ViewData["book"] = new BookModel { Author = "mansi", Id = 3 };
             //var obj = new { Id = 1, name = "smruti" };
-            ViewBag.Title = "Smruti";
-            dynamic data = new ExpandoObject();
-            data.Id = 1;
-            data.Name = "mansi";
+            //ViewBag.Title = "Smruti";
+            //dynamic data = new ExpandoObject();
+            //data.Id = 1;
+            //data.Name = "mansi";
 
-            ViewBag.Data = data;
+            //ViewBag.Data = data;
 
-            ViewBag.Type = new BookModel { Id = 5, Author = "this is Unauthorized" };
+            //ViewBag.Type = new BookModel { Id = 5, Author = "this is Unauthorized" };
 
-           // ViewBag.data = new { Id = 1, name = "smruti" };
+            // ViewBag.data = new { Id = 1, name = "smruti" };
             return View();
         }
 
         public ViewResult Aboutus()
         {
+            Title = "This the aboutus title";
             return View();
         }
         public ViewResult Contactus()
